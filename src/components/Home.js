4a742015d720ab42b435da63dcaa48ea51b98566
Rahul Patil <rp7187655@gmail.com>
2018-09-10 21:21:08 +0530

    Add search field and showing dummy books

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,36 @@
 import React, {Component} from 'react';
 
+const books = ["Math","Science","English","history","Geaography","civic","Marathi","dummy1","dummy2"];
+const filteredBooks = ["Math","Science","English","history","Geaography"];
+
 class Home extends Component {
+    constructor(){
+        super();
+        this.state={
+            searchText:'',
+            books:books
+        };
+        this.handleSearchText = this.handleSearchText.bind(this);
+        this.filterBooks = this.filterBooks.bind(this);
+    }
+    handleSearchText(e){
+        this.setState({ searchText: e.target.value });
+    }
+    filterBooks(e){
+        this.state.searchText=="" ? this.setState({books:books}) : this.setState({books:filteredBooks});
+    }
+    getBooks(){
+        return this.state.books.map(book => <li>{book}</li>);
+    }
     render(){
         return (
-            <p>Home</p>
+            <div>
+                <input type="text" onChange={this.handleSearchText} value={this.state.searchText}/>
+                <button onClick={this.filterBooks}>Search</button>
+                <ul>
+                    {this.getBooks()}
+                </ul>
+            </div>
         )
     }
 }
