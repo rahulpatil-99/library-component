3b21f0282745039579d292bc3b8d33d90c491cc6
Rahul Patil <rp7187655@gmail.com>
2018-09-10 23:21:25 +0530

    Add form for reader sign in

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
 
 class Reader extends Component {
+    constructor(){
+        super();
+        this.state={
+            readerName:''
+        }
+        this.handleReaderName = this.handleReaderName.bind(this);
+        this.submit = this.submit.bind(this);
+    }
+
+    handleReaderName(e){
+        this.setState({readerName: e.target.value});
+    }
+    submit(e){
+        this.setState({readerName:''});
+    }
+
     render(){
         return (
-            <p>Reader</p>
+            <div>
+                <input type="text" onChange={this.handleReaderName} value={this.state.readerName}/>
+                <button onClick={this.submit}>submit</button>
+            </div>
         )
     }
 }
